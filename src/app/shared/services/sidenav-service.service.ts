7e9dd75fd2090b08f0c68eef27bc651af9048657
mrw007 <mr.wahib@gmail.com>
2021-08-18 17:48:36 +0100

    added side nav toggle service

---
+++ src/app/shared/services/sidenav-service.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
+/**
+ * Side Navigation Control Service
+ */
+@Injectable()
+export class SidenavServiceService {
+  /**
+   * Side Navigation Component
+   */
+  private sidenav: MatSidenav | undefined;
+
+  /**
+   * Init the Side Navigation
+   */
+  public setSidenav(sidenav: MatSidenav) {
+    this.sidenav = sidenav;
+  }
+
+  /**
+   * Open Side Navigation
+   */
+  public open() {
+    return this.sidenav?.open();
+  }
+
+  /**
+   * Close Side Navigation
+   */
+  public close() {
+    return this.sidenav?.close();
+  }
+
+  /**
+   * Toggle Side Navigation
+   */
+  public toggle(): void {
+    this.sidenav?.toggle();
+  }
+}
