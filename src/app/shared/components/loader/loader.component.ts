18a75241466381edbb716cc565de0e7e0b4283cb
mrw007 <mr.wahib@gmail.com>
2021-08-19 00:50:33 +0100

    Added Loader component

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,57 @@
+import {
+  AfterViewChecked,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+
+import { LoaderService } from 'src/app/core/services/loader.service';
+import { Subject } from 'rxjs';
+import { ProgressBarMode } from '@angular/material/progress-bar';
+
+/**
+ * Loader Component
+ */
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss'],
+})
+export class LoaderComponent implements AfterViewChecked {
+  /**
+   * Color
+   */
+  color = 'accent';
+
+  /**
+   * Mode
+   */
+  mode: ProgressBarMode = 'determinate';
+
+  /**
+   * Size
+   */
+  value: Subject<number> = this.loaderService.percentage;
+
+  /**
+   * Is Loading
+   */
+  isLoading: Subject<boolean> = this.loaderService.isLoading;
+
+  /**
+   * Constructor
+   * @param loaderService loader Service
+   * @param cdRef cdRef
+   */
+  constructor(
+    private loaderService: LoaderService,
+    private cdRef: ChangeDetectorRef
+  ) {}
+
+  /**
+   * After View Checked
+   */
+  ngAfterViewChecked() {
+    this.cdRef.detectChanges();
+  }
+}
