15a0f7368dfb1ee29e8783d808440aea4fd916b8
mrw007 <mr.wahib@gmail.com>
2021-08-18 18:11:25 +0100

    Added secure module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'secure',
+    loadChildren: () =>
+      import('./secure/secure.module').then((m) => m.SecureModule),
+  },
+  {
+    path: '',
+    redirectTo: 'secure',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
